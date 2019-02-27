e8082f3203fa465ec5f7f306c016011c85b62524
Arockia Benadict <Arockia.Benadict@wdc.com>
2019-02-27 16:59:41 +0530

    Branch1 commit

---
+++ WpfApp1/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+       // public string BookTitle { get; set; }
+        //public bool IsMVP { get; set; }
+    }
+}
